78969f9c29176a73e07032d97c24683037505437
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-15 05:42:15 +0300

    Created TicketList component and conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
 import ticketReducer from "./components/reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
   const initialState = { tickets: [] };
@@ -14,6 +15,17 @@ function App() {
       <div className="container">
         <h1>Bug Blaster</h1>
         <TicketForm dispatch={dispatch}></TicketForm>
+
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+
+            <TicketList
+              tickets={state.tickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
       </div>
     </div>
   );
