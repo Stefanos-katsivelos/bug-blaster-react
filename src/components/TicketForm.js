1219f2ed553a307757eb6bfa791452068d383f5a
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-16 03:36:09 +0300

    Canceling an edit procedure

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -45,6 +45,11 @@ export default function TicketForm({ dispatch, editingTicket }) {
     clearForm();
   };
 
+  const handleCancelEdit = () => {
+    dispatch({ type: "CLEAR_EDITING_TICKET" });
+    clearForm();
+  };
+
   return (
     <form onSubmit={handleSubmit} className="ticket-form">
       <div>
@@ -86,6 +91,12 @@ export default function TicketForm({ dispatch, editingTicket }) {
       <button type="submit" className="button">
         Submit
       </button>
+
+      {editingTicket && (
+        <button className="button" onChange={handleCancelEdit}>
+          Cancel Edit
+        </button>
+      )}
     </form>
   );
 }
