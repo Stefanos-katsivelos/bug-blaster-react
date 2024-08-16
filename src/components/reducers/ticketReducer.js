f4fecbd93adcef43a75ae770519685e70ef919b5
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-16 03:18:22 +0300

    Editing Tickets

--- src/components/reducers/ticketReducer.js
+++ src/components/reducers/ticketReducer.js
@@ -2,6 +2,7 @@ export default function ticketReducer(state, action) {
   switch (action.type) {
     case "ADD_TICKET":
       return { ...state, tickets: [...state.tickets, action.payload] };
+
     case "UPDATE_TICKET":
       return {
         ...state,
@@ -9,12 +10,23 @@ export default function ticketReducer(state, action) {
           ticket.id === action.payload.id ? action.payload : ticket
         ),
       };
+
     case "DELETE_TICKET":
       return {
         ...state,
         tickets: state.tickets.filter(
           (ticket) => ticket.id !== action.payload.id
         ),
+      };
+    case "SET_EDITING_TICKET":
+      return {
+        ...state,
+        editingTicket: action.payload,
+      };
+    case "CLEAR_EDITING_TICKET":
+      return {
+        ...state,
+        editingTicket: null,
       };
     default:
       return state;
